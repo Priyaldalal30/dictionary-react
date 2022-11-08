bc578241ac028d9f79399f442bc8808a62e86138
Priyaldalal30 <112647678+Priyaldalal30@users.noreply.github.com>
2022-11-08 14:15:14 +0000

    added a style card for definition,

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,18 @@
 import React from "react";
 
+import "./Cards.css";
+
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
-    <div className="meaning">
-      <h4>{props.meaning.partOfSpeech}</h4>
-      <p>
+    <div>
+      <h4 className="partOfSpeech">{props.meaning.partOfSpeech}</h4>
+      <p className="definition">
         {props.meaning.definitions.map(function (definition, index) {
           return (
             <div key={index}>
-              <div className="definition">{definition.definition}</div>
-              <p>
+              <div>{definition.definition}</div>
+              <p className="example">
                 <em>{definition.example}</em>
               </p>
             </div>
--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Meaning from "./Meaning";
 
+import "./Cards.css";
+
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
       <div className="results">
-        <h3>{props.results.word}</h3>
+        <h3 className="word">{props.results.word}</h3>
         <p>
           {props.results.meanings.map(function (meaning, index) {
             return (
-              <div key={index}>
+              <div className="resultCard" key={index}>
                 <Meaning meaning={meaning} />
               </div>
             );
