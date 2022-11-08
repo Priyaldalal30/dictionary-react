35cf139e2f85d462f38d0a185c802e6d898a100f
Priyaldalal30 <112647678+Priyaldalal30@users.noreply.github.com>
2022-11-08 22:43:12 +0000

    edited phonetics.js

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -7,4 +7,19 @@ export default function Phonetics(props) {
     const audio = new Audio(sound);
     audio.play();
   }
+
+  if (props.phonetics) {
+    return props.phonetics.map((el) => {
+      return (
+        <div>
+          <div className="audio">
+            <button className="phonetic" onClick={() => playSound(el.audio)}>
+              <i class="fa-regular fa-circle-play"></i>
+            </button>
+          </div>
+          <div className="text">{el.text}</div>
+        </div>
+      );
+    });
+  }
 }
